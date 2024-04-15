9c0d2f4668110fbecd6a2db544faf770824316cc
Nilankar Deb <69686900+Maddy1107@users.noreply.github.com>
2024-04-16 04:01:33 +0530

    Complete

---
+++ main.py
@@ -0,0 +1,28 @@
+# IMPORTING NECESSARY LIBRARIES
+import plotly.express as px
+import mysql.connector as sql
+
+# IMPORT LOCAL MODULES
+import Scripts.sql_script as sql
+import Scripts.streamlit_functions as st
+
+# CONNECT DATABASE
+cursor, mydb = sql.connect_database()
+
+# SETTING PAGE CONFIGURATIONS
+st.set_page_config()
+
+# CREATING OPTION MENU
+selected = st.create_option_menu()
+
+if selected == "Home":
+    st.home(cursor,mydb)
+ 
+if selected == "Insert":
+    st.insert(cursor,mydb)
+
+if selected == "Analysis":
+    st.analysis(cursor,mydb)
+
+if selected == "About":
+    st.about()
